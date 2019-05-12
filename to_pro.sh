752302efdb7987c336e1aad450a5f53eaff8625e
CMouse <qt2126@columbia.edu>
2019-05-12 04:16:27 -0400

    Add scripts for switching between protected and unprotected mode

---
+++ to_pro.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+HOST_IP=10.0.0.3
+REMOTE_IP=10.0.0.4
+NET_MASK=255.0.0.0
+
+# 1. Delete port enp0s3 from s1
+sudo ovs-vsctl del-port s1 enp0s3
+
+# 2. Give enp0s3 same IP address as h1
+sudo ifconfig enp0s3 down
+sudo ifconfig enp0s3 $HOST_IP netmask $NET_MASK
+sudo ifconfig enp0s3 up
+
+# 3. Create tunnel
+sudo ovs-vsctl add-port s1 s1-gre1 -- set interface s1-gre1 type=gre options:remote_ip=$REMOTE_IP
---
+++ to_unpro.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# 1. Remove tunnel
+sudo ovs-vsctl del-port s1 s1-gre1
+
+# 2. Remove address of enp0s3
+sudo ifconfig enp0s3 0
+
+# 3. Add port enp0s3 to s1
+sudo ovs-vsctl add-port s1 enp0s3
