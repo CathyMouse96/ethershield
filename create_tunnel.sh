38df34c5c4c1b79dd46152b6521e3c6f5ab30dfa
CathyMouse96 <qt2126@columbia.edu>
2019-06-09 21:34:27 -0400

    Change mode

--- create_tunnel.sh
+++ create_tunnel.sh
@@ -2,4 +2,4 @@
 
 # Create tunnel
 # Usage: $ create_tunnel <INTERFACE> <REMOTE_IP>
-sudo ovs-vsctl add-port s1 $1 -- set interface s1-gre1 type=gre options:remote_ip=$2 options:psk=swordfish
+sudo ovs-vsctl add-port s1 $1 -- set interface $1 type=gre options:remote_ip=$2 options:psk=swordfish
