1c5851775bbb46185068fce586c248d6152ed06c
CathyMouse96 <qt2126@columbia.edu>
2019-06-09 23:21:11 -0400

    Give IP addr to internal port

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo ovs-vsctl add-port s1 enp0s3
 sudo ifconfig enp0s3 0
+sudo dhclient s1
--- to_unpro.sh
+++ to_unpro.sh
@@ -8,3 +8,6 @@ sudo ifconfig enp0s3 0
 
 # 3. Add port enp0s3 to s1
 sudo ovs-vsctl add-port s1 enp0s3
+
+# 4. Give IP address to internal port s1
+sudo dhclient s1
